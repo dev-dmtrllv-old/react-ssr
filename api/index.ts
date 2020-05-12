49b4a044b3a5d83de2f8e1348c9170038c10269e
dev.dmtrllv <34634620+dev-dmtrllv@users.noreply.github.com>
2020-05-12 19:48:01 +0200

    added comment

--- api/index.ts
+++ api/index.ts
@@ -4,6 +4,7 @@ import type { ClientApi } from "ssr/Api";
 
 let api: ClientApi<ApiRoutesType>;
 
+// this way only the server will contain the api logic
 if (env.isServer)
 	api = Client.createServerApi(require("./routes").apiRoutes);
 else
